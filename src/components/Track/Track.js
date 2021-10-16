fbd4e99d0f88e1bac4a695313ed19dc1da33fee9
James Cole <james@openstack.org>
2021-10-16 17:56:48 -0500

    Edit Track
    Signed-off-by: James Cole <james@openstack.org>

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -3,16 +3,24 @@ import './Track.css';
 
 class Track extends React.Component {
 
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action">-</button>
+    } else {
+      return <button className="Track-action">+</button>
+    }
+  }
+
   render() {
-  return (
-    <div className="Track">
-      <div className="Track-information">
-        <h3><!-- track name will go here --></h3>
-        <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>track name</h3>
+          <p>track artist  |  track album </p>
+        </div>
+        {this.renderAction()}
       </div>
-      <button className="Track-action"><!-- + or - will go here --></button>
-    </div>
-  );
+    );
 }
 }
 
