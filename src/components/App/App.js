57235d74ab648633115643e37e43770286cc0b99
James Cole <james@openstack.org>
2021-10-16 19:07:21 -0500

    Step 61
    Signed-off-by: James Cole <james@openstack.org>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -31,13 +32,17 @@ class App extends React.Component {
       return;
     }
     tracks.push(track);
-    this.setState({playlistTracks: tracks});
+    this.setState({ playlistTracks: tracks });
   }
 
   removeTrack(track) {
     let tracks = this.state.playlistTracks;
     tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
-    this.setState({playlistTracks: tracks});
+    this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
   }
 
   render() {
@@ -54,7 +59,8 @@ class App extends React.Component {
           <Playlist 
           playlistName={this.state.playlistName} 
           playlistTracks={this.state.playlistTracks} 
-          onRemove={this.removeTrack}
+          onRemove={this.removeTrack} 
+          onNameChange={this.updatePlaylistName}
           />
         </div>
       </div>
