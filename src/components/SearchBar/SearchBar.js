0975b9f1a0595c250c24a7e7448c8f5232e37b55
James Cole <james@openstack.org>
2021-10-16 22:40:49 -0500

    Finish project
    Signed-off-by: James Cole <james@openstack.org>

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,8 +9,8 @@ class SearchBar extends React.Component {
       term: ''
     }
 
-    this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
+    this.search = this.search.bind(this);
   }
 
   search() {
@@ -24,8 +24,8 @@ class SearchBar extends React.Component {
   render() {
   return (
     <div className="SearchBar">
-      <input  onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
-      <button className="SearchButton">SEARCH</button>
+      <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+      <button onClick={this.search} className="SearchButton">SEARCH</button>
     </div>
   );
 }
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -5,16 +5,16 @@ import TrackList from '../TrackList/TrackList'
 class SearchResults extends React.Component {
 
   render() {
-  return (
-    <div className="SearchResults">
-      <h2>Results</h2>
-      <TrackList tracks={this.props.searchResults}
-      onAdd={this.props.onAdd}
-      isRemoval={false}
-      />
-    </div>
-  );
-}
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList tracks={this.props.searchResults} 
+        onAdd={this.props.onAdd}
+
+        />
+      </div>
+    );
+  }
 }
 
 export default SearchResults;
